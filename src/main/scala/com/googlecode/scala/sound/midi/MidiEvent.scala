1c4f11b8bc79450e6d24714a3a51c4812f351eae
Eric Olander <eolander@the41st.com>
2012-07-05 16:07:13 -0700

    scaladoc

--- src/main/scala/com/googlecode/scala/sound/midi/MidiEvent.scala
+++ src/main/scala/com/googlecode/scala/sound/midi/MidiEvent.scala
@@ -17,7 +17,17 @@ package com.googlecode.scala.sound.midi
 
 import javax.sound.midi.MidiMessage
 
+/**
+ * Factory object for javax.sound.midi.MidiEvent
+ */
 object MidiEvent {
+
+  /**
+   *
+   * @param msg
+   * @param tick
+   * @return javax.sound.midi.MidiEvent instance
+   */
   def apply(msg: MidiMessage, tick: Long) = {
     new javax.sound.midi.MidiEvent(msg, tick)
   }
